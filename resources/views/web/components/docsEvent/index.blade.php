26359e928ef32229e76479e94fb7decf71c28655
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-08-20 21:05:41 +0700

    update netlify

--- resources/views/web/components/docsEvent/index.blade.php
+++ resources/views/web/components/docsEvent/index.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="theme-tab">
                         <ul class="tabs tab-title">
-                            <li class="current"><a href="tab-7">winter</a></li>
+                            <li class="current"><a href="tab-7">Girlboss with OMG!!</a></li>
                             <li class=""><a href="tab-8">greens</a></li>
                             <li class=""><a href="tab-9">various</a></li>
                         </ul>
@@ -21,13 +21,57 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlboss/omg-1.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="tab-box">
+                                        <div class="product-box2">
+                                            <div class="media">
+                                                <a href="product-page(no-sidebar).html" class="w-100"><img
+                                                        class="img-fluid blur-up lazyload w-100"
+                                                        src="{{ asset('assets/web/images/event/girlboss/omg-2.jpg') }}"
+                                                        alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-box">
+                                        <div class="product-box2">
+                                            <div class="media">
+                                                <a href="product-page(no-sidebar).html" class="w-100"><img
+                                                        class="img-fluid blur-up lazyload w-100"
+                                                        src="{{ asset('assets/web/images/event/girlboss/omg-3.jpg') }}"
+                                                        alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-box">
+                                        <div class="product-box2">
+                                            <div class="media">
+                                                <a href="product-page(no-sidebar).html" class="w-100"><img
+                                                        class="img-fluid blur-up lazyload w-100"
+                                                        src="{{ asset('assets/web/images/event/girlboss/omg-4.jpg') }}"
+                                                        alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-box">
+                                        <div class="product-box2">
+                                            <div class="media">
+                                                <a href="product-page(no-sidebar).html" class="w-100"><img
+                                                        class="img-fluid blur-up lazyload w-100"
+                                                        src="{{ asset('assets/web/images/event/girlboss/omg-5.jpg') }}"
+                                                        alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- <div class="tab-box">
                                         <div class="product-box2">
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
@@ -59,51 +103,7 @@
                                                 </a>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="tab-box">
-                                        <div class="product-box2">
-                                            <div class="media">
-                                                <a href="product-page(no-sidebar).html" class="w-100"><img
-                                                        class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
-                                                        alt="">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-box">
-                                        <div class="product-box2">
-                                            <div class="media">
-                                                <a href="product-page(no-sidebar).html" class="w-100"><img
-                                                        class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
-                                                        alt="">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-box">
-                                        <div class="product-box2">
-                                            <div class="media">
-                                                <a href="product-page(no-sidebar).html" class="w-100"><img
-                                                        class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
-                                                        alt="">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="tab-box">
-                                        <div class="product-box2">
-                                            <div class="media">
-                                                <a href="product-page(no-sidebar).html" class="w-100"><img
-                                                        class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
-                                                        alt="">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    </div> --}}
                                 </div>
                             </div>
                             <div id="tab-8" class="tab-content">
