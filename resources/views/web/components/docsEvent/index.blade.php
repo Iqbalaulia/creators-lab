0f9c7f3a27f4a535ecc8105b8c20cfb18f00d4e1
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-08-24 09:36:20 +0700

    event images cover

--- resources/views/web/components/docsEvent/index.blade.php
+++ resources/views/web/components/docsEvent/index.blade.php
@@ -9,9 +9,9 @@
                     </div>
                     <div class="theme-tab">
                         <ul class="tabs tab-title">
-                            <li class="current"><a href="tab-7">Girlboss with OMG!!</a></li>
+                            <li class="current"><a href="tab-7">girlBoss with OMG!!</a></li>
                             <li class=""><a href="tab-8">Discover the magic of lip suede</a></li>
-                            <li class=""><a href="tab-9">various</a></li>
+                            <li class=""><a href="tab-9">International Lipstick Days</a></li>
                         </ul>
                         <div class="tab-content-cls">
                             <div id="tab-7" class="tab-content active default">
@@ -21,7 +21,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-1.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-1.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -32,7 +32,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-2.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-2.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -43,7 +43,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-3.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-3.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -54,7 +54,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-4.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-4.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -65,7 +65,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-5.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-5.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -76,7 +76,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-6.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-6.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -87,7 +87,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-7.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -98,7 +98,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/event/girlboss/omg-8.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/girlBoss/omg-8.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -205,7 +205,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-1.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -216,7 +216,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-2.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -227,7 +227,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-3.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -238,7 +238,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-4.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -249,7 +249,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-5.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -260,7 +260,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-6.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -271,7 +271,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-7.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
@@ -282,7 +282,7 @@
                                             <div class="media">
                                                 <a href="product-page(no-sidebar).html" class="w-100"><img
                                                         class="img-fluid blur-up lazyload w-100"
-                                                        src="{{ asset('assets/web/images/flower/product/7.jpg') }}"
+                                                        src="{{ asset('assets/web/images/event/internationalLips/wrd-8.jpg') }}"
                                                         alt="">
                                                 </a>
                                             </div>
