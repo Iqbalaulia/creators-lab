fe8f15d1bb519bcbca0aa149a1742bdfe041d145
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-08-24 20:58:23 +0700

    fix

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
-
+    {{--  --}}
     <!-- Icons -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/web/css/vendors/font-awesome.css') }}">
 
